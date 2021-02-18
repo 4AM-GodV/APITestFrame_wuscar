166beff8c9f0619b0e421a68a52a4cfdde4663cf
4AM-GodV <74580031+4AM-GodV@users.noreply.github.com>
2021-02-18 15:16:57 +0800

    nodemo

--- demo/configdemo.py
+++
@@ -1,8 +0,0 @@
-import os
-import configparser
-
-config_path = os.path.join( os.path.dirname(__file__), '..', 'conf/config.ini')
-
-cfg = configparser.ConfigParser
-cfg.read(config_path)
-print(config_path)
